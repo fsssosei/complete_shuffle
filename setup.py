5d6ff62b1a436c1ef4ce57defe6ab66ad3b11917
fsssosei <43291778+fsssosei@users.noreply.github.com>
2020-10-31 06:36:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", encoding='utf-8') as fh:
 
 setup(
     name='complete_shuffle',
-    version='0.9.3',
+    version='0.9.4',
     description='Complete shuffling of lists with true random or pseudo random sequences.',
     long_description=long_description,
     long_description_content_type='text/markdown',
